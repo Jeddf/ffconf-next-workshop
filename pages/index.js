a01c8a55bef9c550690c0d5acde026b861d570ed
Jedd Fenner <jedd@outlook.com>
2019-11-07 11:36:47 +0000

    speakers data and listing out on home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-import Layout from '../components/Layout';
-export default () => (
+import Link from "next/link";
+import Layout from "../components/Layout";
+import sessions from "../data/sessions.json";
+
+const Page = () => (
   <Layout>
-    <h1>Welcome</h1>
-    <p>etc...</p>
+    <h1>Talks</h1>
+    <ul>
+      {sessions.map(session => (
+        <li key={session.slug}>
+          <p className="title">
+            <Link href={`/session?slug=${session.slug}`}>
+              <a>{session.title}</a>
+            </Link>
+          </p>
+          <p className="info">
+            <span>⏰ {session.time}</span>
+            <span className="speaker name">{session.speaker.name} </span>
+            <span className="speaker twitter">
+              <a
+                href={`https://twitter.com/${session.speaker.twitter}`}
+                target="_blank"
+              >
+                @{session.speaker.twitter}
+              </a>
+            </span>
+          </p>
+        </li>
+      ))}
+    </ul>
   </Layout>
 );
+
+export default Page;
