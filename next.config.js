d765cd5b7df814932f2f65e443489df74bb9cbfe
Jedd Fenner <jedd@outlook.com>
2019-11-07 16:52:29 +0000

    .mdx support, hidden /aboot page

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 const path = require("path");
 
-module.exports = {
+const withMDX = require("@zeit/next-mdx")({
+  extension: /\.mdx?$/
+});
+
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "mdx", "md"],
   webpack: config => {
     config.resolve.alias["~"] = path.resolve(__dirname);
     return config;
   }
-};
+});
---
+++ pages/aboot.md
@@ -0,0 +1,5 @@
+# About (in pure markdown)
+
+I hail from Brighton. This workshop is googly-eyes good, and I'm doing all the Next.js things ❤️.
+
+[Contact me](/contact) to get in touch
