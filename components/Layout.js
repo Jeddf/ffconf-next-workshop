d7a7fdf3f2f8e857530b837916819b5b51157311
Jedd Fenner <jedd@outlook.com>
2019-11-07 14:53:06 +0000

    loading progress bar everywhere

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,12 @@
-import Header from './Header';
-import Footer from './Footer';
+import Header from "./Header";
+import Footer from "./Footer";
+import router from "next/router";
+import NProgress from "nprogress";
+
+router.onRouteChangeStart = () => NProgress.start();
+router.onRouteChangeComplete = () => NProgress.done();
+router.onRouteChangeError = () => NProgress.done();
+
 export default ({ children, ...props }) => (
   <div id="root">
     <Header {...props} />
